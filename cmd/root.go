9d3637812abd4bb3660982900a083b5da11cd7bd
Shi Han NG <shihanng@gmail.com>
2018-07-29 00:27:04 +0900

    use cobra

---
+++ cmd/root.go
@@ -0,0 +1,200 @@
+// Copyright © 2018 Shi Han NG
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/google/go-github/github"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/oauth2"
+	billy "gopkg.in/src-d/go-billy.v4"
+	"gopkg.in/src-d/go-billy.v4/memfs"
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
+)
+
+var (
+	public      bool
+	description string
+	accessToken string
+
+	dummyFilename github.GistFilename = "dummy.go"
+	dummyContent                      = "package dummy"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bgist",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.MinimumNArgs(1),
+	RunE: actual,
+}
+
+func actual(cmd *cobra.Command, args []string) error {
+	user, gitPullURL, err := createGist(description, public)
+	if err != nil {
+		return err
+	}
+
+	fs := memfs.New()
+	storer := memory.NewStorage()
+
+	r, err := git.Clone(storer, fs, &git.CloneOptions{
+		URL: gitPullURL,
+	})
+	if err != nil {
+		return errors.Wrap(err, "clone gist to memory")
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return errors.Wrap(err, "getting the worktree")
+	}
+
+	for _, f := range args {
+		filename, err := addFile(f, fs)
+		if err != nil {
+			return err
+		}
+
+		_, err = w.Add(filename)
+		if err != nil {
+			return errors.Wrap(err, "git add new filename")
+		}
+	}
+
+	if _, err := w.Remove(string(dummyFilename)); err != nil {
+		return errors.Wrap(err, "git rm")
+	}
+
+	_, err = w.Commit("", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  user.GetName(),
+			Email: user.GetEmail(),
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "git commit")
+	}
+
+	auth := &http.BasicAuth{Username: user.GetLogin(), Password: accessToken}
+
+	if err := r.Push(&git.PushOptions{Auth: auth}); err != nil {
+		return errors.Wrap(err, "git push")
+	}
+
+	return nil
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&public, "public", false, "Publish as public gist")
+	rootCmd.PersistentFlags().StringVarP(&description, "description", "d", "", "Description of the gist")
+
+	viper.SetEnvPrefix("bgist")
+	if err := viper.BindEnv("github_access_token"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	accessToken = viper.GetString("github_access_token")
+	if accessToken == "" {
+		fmt.Println(`GitHub's personal access token is needed as environment variable BGIST_GITHUB_ACCESS_TOKEN.`)
+		fmt.Println(`It can be obtained from https://github.com/settings/tokens. The require scope is "gist".`)
+		os.Exit(1)
+	}
+}
+
+func createGist(description string, public bool) (*github.User, string, error) {
+	ctx := context.Background()
+
+	auth := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	))
+
+	client := github.NewClient(auth)
+
+	g := github.Gist{
+		Description: &description,
+		Public:      &public,
+		Files: map[github.GistFilename]github.GistFile{
+			dummyFilename: github.GistFile{
+				Content: &dummyContent,
+			},
+		},
+	}
+
+	gists, _, err := client.Gists.Create(ctx, &g)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "create new gist")
+	}
+	fmt.Println("created:", gists.GetHTMLURL())
+
+	return gists.GetOwner(), gists.GetGitPullURL(), nil
+}
+
+func addFile(source string, destination billy.Filesystem) (string, error) {
+	filename := filepath.Base(source)
+
+	newFile, err := destination.Create(filename)
+	if err != nil {
+		return "", errors.Wrap(err, "create new file in gist fs")
+	}
+	defer newFile.Close()
+
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return "", errors.Wrap(err, "open new file")
+	}
+	defer sourceFile.Close()
+
+	_, err = io.Copy(newFile, sourceFile)
+	if err != nil {
+		return "", errors.Wrap(err, "copy the file to gist fs")
+	}
+
+	return filename, nil
+}
--- main.go
+++ main.go
@@ -1,159 +1,27 @@
+// Copyright © 2018 Shi Han NG
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package main
 
-import (
-	"context"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"time"
-
-	"github.com/google/go-github/github"
-	"github.com/pkg/errors"
-	flag "github.com/spf13/pflag"
-	"golang.org/x/oauth2"
-	billy "gopkg.in/src-d/go-billy.v4"
-	"gopkg.in/src-d/go-billy.v4/memfs"
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing/object"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
-	"gopkg.in/src-d/go-git.v4/storage/memory"
-)
-
-var (
-	accessToken = os.Getenv("BGIST_GITHUB_ACCESS_TOKEN")
-
-	public      bool
-	description string
-
-	dummyFilename github.GistFilename = "dummy.go"
-	dummyContent                      = "package dummy"
-)
-
-func init() {
-	if accessToken == "" {
-		die("BGIST_GITHUB_ACCESS_TOKEN is empty")
-	}
-
-	flag.BoolVar(&public, "public", false, "publish as public gist")
-	flag.StringVarP(&description, "description", "d", "", "description of the gist")
-
-	flag.ErrHelp = nil
-}
-
-func die(v interface{}) {
-	fmt.Fprintf(os.Stderr, "%v\n", v)
-	os.Exit(1)
-}
+import "github.com/shihanng/bgist/cmd/cmd"
 
 func main() {
-	flag.Parse()
-
-	if flag.NArg() < 1 {
-		die("need at least one file as argument")
-	}
-
-	user, gitPullURL, err := createGist(description, public)
-	if err != nil {
-		die(err)
-	}
-
-	fs := memfs.New()
-	storer := memory.NewStorage()
-
-	r, err := git.Clone(storer, fs, &git.CloneOptions{
-		URL: gitPullURL,
-	})
-	if err != nil {
-		die(errors.Wrap(err, "clone gist to memory"))
-	}
-
-	w, err := r.Worktree()
-	if err != nil {
-		die(errors.Wrap(err, "getting the worktree"))
-	}
-
-	for _, f := range flag.Args() {
-		filename, err := addFile(f, fs)
-		if err != nil {
-			die(err)
-		}
-
-		_, err = w.Add(filename)
-		if err != nil {
-			die(errors.Wrap(err, "git add new filename"))
-		}
-	}
-
-	if _, err := w.Remove(string(dummyFilename)); err != nil {
-		die(errors.Wrap(err, "git rm"))
-	}
-
-	_, err = w.Commit("", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  user.GetName(),
-			Email: user.GetEmail(),
-			When:  time.Now(),
-		},
-	})
-	if err != nil {
-		die(errors.Wrap(err, "git commit"))
-	}
-
-	auth := &http.BasicAuth{Username: user.GetLogin(), Password: accessToken}
-
-	if err := r.Push(&git.PushOptions{Auth: auth}); err != nil {
-		die(errors.Wrap(err, "git push"))
-	}
-}
-
-func createGist(description string, public bool) (*github.User, string, error) {
-	ctx := context.Background()
-
-	auth := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: accessToken},
-	))
-
-	client := github.NewClient(auth)
-
-	g := github.Gist{
-		Description: &description,
-		Public:      &public,
-		Files: map[github.GistFilename]github.GistFile{
-			dummyFilename: github.GistFile{
-				Content: &dummyContent,
-			},
-		},
-	}
-
-	gists, _, err := client.Gists.Create(ctx, &g)
-	if err != nil {
-		return nil, "", errors.Wrap(err, "create new gist")
-	}
-	fmt.Println("created:", gists.GetHTMLURL())
-
-	return gists.GetOwner(), gists.GetGitPullURL(), nil
-}
-
-func addFile(source string, destination billy.Filesystem) (string, error) {
-	filename := filepath.Base(source)
-
-	newFile, err := destination.Create(filename)
-	if err != nil {
-		return "", errors.Wrap(err, "create new file in gist fs")
-	}
-	defer newFile.Close()
-
-	sourceFile, err := os.Open(source)
-	if err != nil {
-		return "", errors.Wrap(err, "open new file")
-	}
-	defer sourceFile.Close()
-
-	_, err = io.Copy(newFile, sourceFile)
-	if err != nil {
-		return "", errors.Wrap(err, "copy the file to gist fs")
-	}
-
-	return filename, nil
+	cmd.Execute()
 }
